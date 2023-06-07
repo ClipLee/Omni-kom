09602dbd9e0813a8bef99dd34a5e791c5bac9c59
LinDen <k.lipski777@gmail.com>
2023-06-08 01:24:01 +0200

    first commit

---
+++ src/main/java/mas/MAS_Projekt/MasProjektApplication.java
@@ -0,0 +1,13 @@
+package mas.MAS_Projekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasProjektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasProjektApplication.class, args);
+	}
+
+}
